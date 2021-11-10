5f84ad90733273f9289f97d7bd3041edd6bb068c
isouffrant1 <60717996+isouffrant1@users.noreply.github.com>
2021-11-10 08:37:57 -0500

    Create foo.java

---
+++ foo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class foo
+{
+public static void main(String args[])
+{
+int x=0;
+int i=1;
+while(i<=3)
+{
+int s = i*i;
+x = s + x;
+i = i + 1;
+}
+System.out.println("x = "+x);
+}
+}
