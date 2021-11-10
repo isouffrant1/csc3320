1ee4aa3732b669697194466d6ea27d58584d4c67
isouffrant1 <60717996+isouffrant1@users.noreply.github.com>
2021-11-10 08:35:54 -0500

    Create hello.sh

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Welcome to computer science society"
+#Iven Souffrant
+#isouffrant1@student.gsu.edu
+echo "Date is = `date`"
+echo */ | wc
+echo $PATH
+echo $USER
+echo $SHELL
+echo "Printing disk usage"
+echo "`df`"
+echo "Could you loan me "$"25.00?"
+echo "if x = 2, x*x = 4, x/2 = 1"
+echo "`ls * .sh | grep c`"
+echo "Good Bye"
+echo "Current Hour = `date +"%H"`"
