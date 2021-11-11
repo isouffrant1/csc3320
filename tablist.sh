6d8562423b60ba5053fc103bd9145a5b4fd1d896
isouffrant1 <60717996+isouffrant1@users.noreply.github.com>
2021-11-11 16:31:30 -0500

    Create tablist.sh

---
+++ tablist.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+function find_statements() {
+
+        i=0
+
+        while IFS=. read -r -a line
+        do
+        IFS="."
+
+                for stat in ${line[@]}
+                do
+
+                        statements[i]=$(echo $stat | sed 's/^ //g')
+
+                        i=$((i+1))
+                done
+        done < $1
+
+
+        echo -e "\t\tWords\tletters"
+
+i=1
+
+
+        for statement in ${statements[@]}
+        do
+
+                words=$(echo $statement | wc -w)
+                letters=$(echo $statement | tr -d ' ' | wc -c)
+
+                echo -e "Statement $i:\t  $words\t  $((letters-1))"
+
+                i=$((i+1))
+        done
+}
+
+
+if(($#==1))
+then
+
+        file=$1
+else
+
+        echo -n "Text file: "
+read file
+fi
+
+
+if [ -f $file ]
+then
+
+        find_statements $file
+else
+
+        echo "$file: file not exist"
+        exit 1
+fi
